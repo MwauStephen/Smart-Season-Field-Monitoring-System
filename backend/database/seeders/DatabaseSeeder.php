b5f5a7a7d74294074b52a177d21f1558bbc4d67a
Mwau Stephen <stephenmwau41@gmail.com>
2026-04-21 00:52:33 +0300

    created database seeders to have mock data for testing

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -15,11 +15,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory()->create([
+            'name' => 'Admin User',
+            'email' => 'admin@test.com',
+            'password' => bcrypt('password'),
+            'role' => 'admin',
+        ]);
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Agent User',
+            'email' => 'agent@test.com',
+            'password' => bcrypt('password'),
+            'role' => 'agent',
         ]);
     }
 }
