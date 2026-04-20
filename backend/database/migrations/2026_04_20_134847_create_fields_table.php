05c5bd16b5c20921da6d56efb4b59dcaa03684a7
Mwau Stephen <stephenmwau41@gmail.com>
2026-04-21 00:30:21 +0300

    created the fields migrations

---
+++ backend/database/migrations/2026_04_20_134847_create_fields_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('fields', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('crop_type');
+            $table->date('planting_date');
+            $table->enum('stage', ['Planted', 'Growing', 'Ready', 'Harvested'])->default('Planted');
+            $table->foreignId('assigned_agent_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('created_by')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('fields');
+    }
+};
