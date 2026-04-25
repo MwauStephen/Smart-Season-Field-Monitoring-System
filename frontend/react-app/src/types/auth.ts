77f8f0be4cf3b4d2d70fc8c35c53e4fe34cd90b9
Mwau Stephen <stephenmwau41@gmail.com>
2026-04-25 03:26:05 +0300

    created auth types

---
+++ frontend/react-app/src/types/auth.ts
@@ -0,0 +1,19 @@
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  role: string;
+  email_verified_at?: string | null;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface LoginCredentials {
+  email: string;
+  password: string;
+}
+
+export interface AuthResponse {
+  user: User;
+  token: string;
+}
