6592d44d6d77461726894fefc4b95012eb727de9
Mwau Stephen <stephenmwau41@gmail.com>
2026-04-25 12:10:45 +0300

    created a sheet component

--- frontend/react-app/src/components/ui/sheet.tsx
+++ frontend/react-app/src/components/ui/sheet.tsx
@@ -49,7 +49,9 @@ const sheetVariants = cva(
 
 interface SheetContentProps
   extends React.ComponentPropsWithoutRef<typeof SheetPrimitive.Content>,
-    VariantProps<typeof sheetVariants> {}
+    VariantProps<typeof sheetVariants> {
+  side?: "top" | "bottom" | "left" | "right"
+}
 
 const SheetContent = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Content>,
